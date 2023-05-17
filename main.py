108035c9854cb5e8eb4dd488f97793f2bbcf4ed0
Anton Ralavets <a.ralavets@softarex.com>
2023-05-18 00:57:26 +0300

    add exchange instruments scrapping

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from util.symbols import Symbols
+
+
+def main():
+    print("application started")
+
+    all_symbols = Symbols.extract_symbols()
+    usdt_symbols = Symbols.filter_usdt_symbols(all_symbols)
+
+    i = 0
+    size = len(usdt_symbols)
+    for symbol in usdt_symbols:
+        symbol_info = Symbols.extract_symbol_info(symbol)
+        print(f"[{++i}/{size}] stats for symbol {symbol} was received")
+
+
 if __name__ == '__main__':
-    pass
+    main()
---
+++ util/symbols.py
@@ -0,0 +1,38 @@
+import time
+import requests
+
+from const.url import URL
+
+
+class Symbols:
+
+    @staticmethod
+    def is_usdt_symbol(symbol: str) -> bool:
+        if symbol.endswith("USDT"):
+            return True
+        return False
+
+    @staticmethod
+    def filter_usdt_symbols(symbols: list) -> list:
+        return list(filter(Symbols.is_usdt_symbol, symbols))
+
+    @staticmethod
+    def extract_symbols() -> list:
+        response = requests.get(URL.EXCHANGE_INFO_URL)
+        exchange_info = response.json()
+        return [symbol["symbol"] for symbol in exchange_info["symbols"]]
+
+    @staticmethod
+    def extract_symbol_info(symbol: str) -> object:
+        params = {"symbol": symbol}
+        response = requests.get(URL.SYMBOL_STATS_URL, params=params)
+
+        if response.status_code == 429:
+            # used_weight_1m = response.headers.get("x-mbx-used-weight-1m")
+            retry_after_seconds = int(response.headers.get("Retry-After")) + 1
+
+            print(f"request weight limit has been reached. wait {retry_after_seconds} seconds")
+            time.sleep(retry_after_seconds)
+
+        assert response.status_code == 200
+        return response.json()

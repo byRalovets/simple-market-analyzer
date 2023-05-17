1ee199924a03a694454919f8bfa4791a01931a79
Anton Ralavets <a.ralavets@softarex.com>
2023-05-18 01:25:02 +0300

    add db connection & data persistence

--- const/database.py
+++ const/database.py
@@ -4,3 +4,4 @@ class Database:
     REDIS_DB = 0
     REDIS_CHARSET = "utf-8"
     REDIS_HASH_NAME = "market-analyzer"
+    REDIS_PASSWORD_VAR_NAME = 'REDIS_PASSWORD'
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import json
+import redis
+import os
+
+from const.database import Database
 from util.symbols import Symbols
 
 
 def main():
     print("application started")
+    redis_client = redis.Redis(host=Database.REDIS_HOST,
+                               port=Database.REDIS_PORT,
+                               db=Database.REDIS_DB,
+                               password=os.environ[Database.REDIS_PASSWORD_VAR_NAME],
+                               charset=Database.REDIS_CHARSET,
+                               decode_responses=True)
+    print("redis connected")
 
     all_symbols = Symbols.extract_symbols()
     usdt_symbols = Symbols.filter_usdt_symbols(all_symbols)
@@ -11,7 +23,10 @@ def main():
     size = len(usdt_symbols)
     for symbol in usdt_symbols:
         symbol_info = Symbols.extract_symbol_info(symbol)
-        print(f"[{++i}/{size}] stats for symbol {symbol} was received")
+        redis_client.hset(Database.REDIS_HASH_NAME, symbol, json.dumps(symbol_info))
+
+        print(f"[{++i}/{size}] stats for symbol {symbol} was persisted"
+              f" to redis database ({Database.REDIS_HASH_NAME}/{symbol})")
 
 
 if __name__ == '__main__':
